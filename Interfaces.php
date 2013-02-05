47619d771d97ea8c29f509931bf557fe4fb946bd
Phillip Shipley <phillip.shipley@gmail.com>
2013-02-05 10:38:11 -0800

    Renamed Session to Meeting

--- Interfaces.php
+++ Interfaces.php
@@ -2,27 +2,27 @@
 
 namespace Smx\SimpleMeetings;
 
-interface Session
+interface Meeting
 {
-    public function createSession();
-    public function getSessionDetails();
-    public function getSessionList();
-    public function startSession($urlOnly=false);
-    public function joinSession($urlOnly=false);
-    public function editSession();
-    public function deleteSession();
-    public function getActiveSessions();
+    public function createMeeting();
+    public function getMeetingDetails();
+    public function getMeetingList();
+    public function startMeeting($urlOnly=false);
+    public function joinMeeting($urlOnly=false);
+    public function editMeeting();
+    public function deleteMeeting();
+    public function getActiveMeetings();
     public function getRecordingList();
     public function addAttendee();
     public function getAttendeeList();
-    public function getSessionHistory();
+    public function getMeetingHistory();
     public function getAttendeeHistory();
     public function setOptions($options);
     public function setOption($name,$value);
     public function getOption($name);
 }
 
-interface SessionList extends \Iterator
+interface MeetingList extends \Iterator
 {
     public function getArray();
 }
